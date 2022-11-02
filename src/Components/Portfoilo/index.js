9e68be0e0093449b15e124681cef0b929bb31d7c
Mihlayonke <mihlatembe@outlook.com>
2022-11-02 12:54:24 +0200

    Updated components CSS

--- src/Components/Portfoilo/index.js
+++ src/Components/Portfoilo/index.js
@@ -5,6 +5,7 @@ import "./index.scss";
 import Logo from '../../../src/assets/images/Logo.png';
 import Quize from '../../../src/assets/images/Quize.png';
 import Desktop from '../../../src/assets/images/Desktop_App.jpeg';
+//import { Link } from 'react-router-dom';
 
 
 const Portfolio = () => { 
@@ -14,6 +15,7 @@ const Portfolio = () => {
             <div >
                 <h1 className="page-title"> Projects </h1>
             </div>
+
             <div className="container portfolio-page">
                 
                 <div className="image-box">
@@ -26,10 +28,9 @@ const Portfolio = () => {
                         <p className="title">My Portfoilo</p>
                         <h4 className="description">This is portfolio projects that you are currently on it.</h4>
 
-                        <button  target= "_blank" rel="noreferrer"
-                            className="btn"
-                            href="https://github.com/Mihlayonke/Portfolio"
-                       > View Source Code </button>
+                        <a target= "_blank" rel="noreferrer" href='https://github.com/Mihlayonke/Portfolio'>
+                            <button className="btn"> View Source Code </button>
+                        </a>
                     </div>
                 </div>
                 
@@ -43,10 +44,9 @@ const Portfolio = () => {
                         <p className="title">The Quize Game</p>
                         <h4 className="description">This is a quzie game</h4>
 
-                        <button  target= "_blank" rel="noreferrer"
-                            className="btn"
-                            href="https://github.com/Mihlayonke/Quiz_Game"
-                       > View Source Code </button>
+                       <a target= "_blank" rel="noreferrer" href="https://github.com/Mihlayonke/Quiz_Game">
+                            <button className="btn"> View Source Code </button>
+                        </a>
                     </div>
                 </div>
                 
@@ -60,13 +60,17 @@ const Portfolio = () => {
                         <p className="title">Varsity Pharmacy</p>
                         <h4 className="description">This is a POS desktop app for a Varsity Pharmacy store.</h4>
 
-                        <button  target= "_blank" rel="noreferrer"
-                            className="btn"
-                            href="https://github.com/Mihlayonkehttps://github.com/Mihlayonke/Varsity_Pharmacy_Windows_Form/"
-                       > View Source Code </button>
+                       <a target= "_blank" rel="noreferrer" href="https://github.com/Mihlayonke/Varsity_Pharmacy_Windows_Form/">
+                            <button className="btn"> View Source Code </button>
+                        </a>
                     </div>
                 </div>
             </div>
+
+            <div>
+                 
+            </div>
+           
             <Loader type="pacman" />
         </>
     );
