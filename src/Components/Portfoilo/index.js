592d58c96ed8a70f827a159800b41cc71f8cd6b1
Mihlayonke <mihlatembe@outlook.com>
2022-11-08 02:46:40 +0200

    updated projects

--- src/Components/Portfoilo/index.js
+++ src/Components/Portfoilo/index.js
@@ -3,7 +3,7 @@ import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 import Logo from '../../../src/assets/images/Logo.png';
-import Quize from '../../../src/assets/images/Quize.png';
+import Web_App from '../../../src/assets/images/Pharmacy Web.jpeg';
 import Desktop from '../../../src/assets/images/Desktop_App.jpeg';
 //import { Link } from 'react-router-dom';
 
@@ -45,13 +45,13 @@ const Portfolio = () => {
                 
                 <div className="image-box">
                     <img 
-                    src={Quize}
+                    src={Web_App}
                     className="portfolio-image"
                     alt="portfolio" />
                     
                     <div className="content">
-                        <p className="title">The Quize Game</p>
-                        <h4 className="description">This is a quzie game</h4>
+                        <p className="title">The Varsity Pharmacy</p>
+                        <h4 className="description">This is a web applicaion for a pharmacy store</h4>
 
                        <a target= "_blank" rel="noreferrer" href="https://github.com/Mihlayonke/Quiz_Game">
                             <button className="btn"> View Source Code </button>
@@ -67,7 +67,7 @@ const Portfolio = () => {
                     
                     <div className="content">
                         <p className="title">Varsity Pharmacy</p>
-                        <h4 className="description">This is a POS desktop app for a Varsity Pharmacy store.</h4>
+                        <h4 className="description">This is a POS desktop app for a pharmacy store.</h4>
 
                        <a target= "_blank" rel="noreferrer" href="https://github.com/Mihlayonke/Varsity_Pharmacy_Windows_Form/">
                             <button className="btn"> View Source Code </button>
